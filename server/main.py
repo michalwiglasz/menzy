593ad4db227165d618f8acea83146670ce18cce0
Michal Wiglasz <michal.wiglasz@gmail.com>
2012-11-05 19:14:04 +0100

    Allow specify port number and production mode

--- server/main.py
+++ server/main.py
@@ -2,9 +2,17 @@
 
 from flask import Flask
 from menzy.api import api
+import argparse
+
 
 app = Flask(__name__)
 app.register_blueprint(api, url_prefix='/api')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--port', metavar='N', type=int)
+    parser.add_argument('--production', action='store_true')
+    args = parser.parse_args()
+
+    debug = not args.production
+    app.run(host='0.0.0.0', port=args.port, debug=debug)
