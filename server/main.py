a17247614569199288797ebb84f1793e3ae40a60
Michal Wiglasz <michal.wiglasz@gmail.com>
2012-11-05 19:34:04 +0100

    Allow specify base url

--- server/main.py
+++ server/main.py
@@ -5,15 +5,18 @@ from menzy.api import api
 import argparse
 
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--port', metavar='N', type=int)
+parser.add_argument('--production', action='store_true')
+parser.add_argument('--host', type=str, default='0.0.0.0')
+parser.add_argument('--url', type=str, default='/api')
+args = parser.parse_args()
+
+
 app = Flask(__name__)
-app.register_blueprint(api, url_prefix='/api')
+app.register_blueprint(api, url_prefix=args.url)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--port', metavar='N', type=int)
-    parser.add_argument('--production', action='store_true')
-    parser.add_argument('--host', type=str, default='0.0.0.0')
-    args = parser.parse_args()
 
     debug = not args.production
     app.run(host=args.host, port=args.port, debug=debug)
